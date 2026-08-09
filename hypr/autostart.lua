922774db8ddfd90d5db0eb4e4579b594edd1a553
Felixgamer27 <felixgamer27.macrodroid@gmail.com>
2026-08-09 20:17:21 -0300

    Added apps to autostart

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -4,12 +4,16 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
-
  hl.on("hyprland.start", function ()
    hl.exec_cmd("nm-applet")
    hl.exec_cmd("snappy-switcher --daemon")
-   hl.exec_cmd("waybar & hyprpaper & dunst & xdg-desktop-portal-hyprland & hyprpolkitagent & elephant")
+   hl.exec_cmd("waybar")
+   hl.exec_cmd("hyprpaper")
+   hl.exec_cmd("dunst")
+   hl.exec_cmd("xdg-desktop-portal-hyprland")
+   hl.exec_cmd("hyprpolkitagent")
+   hl.exec_cmd("elephant")
+   hl.exec_cmd("kdeconnect-indicator")
+   hl.exec_cmd("kdeconnectd")
+   hl.exec_cmd("hyprpm reload -n")
  end)
-
