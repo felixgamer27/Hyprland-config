bdc35f3a126f65e4452043a0306032f8bb102344
Felixgamer27 <felixgamer27.macrodroid@gmail.com>
2026-08-09 20:18:07 -0300

    Added requires

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -7,33 +7,24 @@
 -------------
 -- Require --
 -------------
-require("keybinds")
-require("autostart")
+
+-- Input --
 require("input")
-require("monitors")
-require("my_programs")
+require("keybinds")
+require("gestures")
+
+-- Theming --
 require("look_and_feel")
 require("windows_and_workspaces")
+require("hyprbar")
+require("sfx")
+
+-- Misc --
+require("autostart")
+require("monitors")
+require("my_programs")
 require("environment_variables")
-
------------------------
------ PERMISSIONS -----
------------------------
-
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
--- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
--- for security reasons
-
--- hl.config({
---   ecosystem = {
---     enforce_permissions = true,
---   },
--- })
-
--- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
--- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
--- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
-
+require("permissions")
 
 
 ----------------
@@ -42,7 +33,7 @@ require("environment_variables")
 
 hl.config({
     misc = {
-        force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
+        force_default_wallpaper = 0,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
     },
 })
